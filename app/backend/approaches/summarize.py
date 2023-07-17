38326660c5382e7cf3cbda1d6422e60e271c75b1
michael.jaumann <michael.jaumann@muenchen.de>
2023-07-17 16:23:49 +0200

    Summarize, then translate

--- app/backend/approaches/summarize.py
+++ app/backend/approaches/summarize.py
@@ -11,11 +11,17 @@ class Summarize(Approach):
     """
     Simple summarize implementation. One shot summarization of the given text.
     """
-    system_message_prompt = """You are a helpful assistant for text summarization. Answer in {language}. """
-    user_message_prompt = """Summarize this in two sentences for a {person_type}: 
-    
-    {text}"""
+    system_sum_prompt = """You are a helpful assistant for text summarization. """
+    user_sum_prompt = """
+    Summarize the following text for a {person_type}.
+    The summary can be up to 3 sentences in length, expressing the key points and concepts written in the original text without adding your interpretations. 
 
+    Text: {text}"""
+
+    system_translate_prompt = """Agiere als {language}-Übersetzer, du übersetzt kommentarlos jede Eingabe. Verstanden?"""
+    user_translate_prompt = """
+    Übersetze den folgenden Text in {language}.
+    Text: {text}"""
 
     def __init__(self, chatgpt_deployment: str, chatgpt_model: str):
         self.chatgpt_deployment = chatgpt_deployment
@@ -32,11 +38,11 @@ class Summarize(Approach):
             messages= [
                 {
                 "role": self.SYSTEM,
-                "content": self.system_message_prompt.format(language=language),
+                "content": self.system_sum_prompt.format()
                 },
                 {
                 "role": self.USER,
-                "content": self.user_message_prompt.format(person_type=person_type, text=text)
+                "content": self.user_sum_prompt.format(person_type=person_type, text=text, language=language)
                 },
             ], 
             temperature=overrides.get("temperature") or 0.7, 
@@ -45,6 +51,25 @@ class Summarize(Approach):
         
         chat_content = chat_completion.choices[0].message.content
 
+        chat_completion = openai.ChatCompletion.create(
+            deployment_id=self.chatgpt_deployment,
+            model=self.chatgpt_model,
+            messages= [
+                {
+                "role": self.SYSTEM,
+                "content": self.system_translate_prompt.format(language=language)
+                },
+                {
+                "role": self.USER,
+                "content": self.user_translate_prompt.format(language=language, text = chat_content)
+                },
+            ], 
+            temperature=overrides.get("temperature") or 0.7, 
+            max_tokens=1024, 
+            n=1)
+
+
+        chat_content = chat_completion.choices[0].message.content
         #msg_to_display = '\n\n'.join([str(message) for message in messages])
 
         return {"data_points": [], "answer": chat_content, "thoughts": f"Searched for:<br>{text}<br><br>Conversations:<br>"} #+ msg_to_display.replace('\n', '<br>')}

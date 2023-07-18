ac267bc706626c828c930aabfd8a27b73dedcecf
michael.jaumann <michael.jaumann@muenchen.de>
2023-07-18 15:36:14 +0200

    Längere Fragen 🚵‍♀️

--- app/frontend/src/components/QuestionInput/QuestionInput.tsx
+++ app/frontend/src/components/QuestionInput/QuestionInput.tsx
@@ -36,7 +36,7 @@ export const QuestionInput = ({ onSend, disabled, placeholder, clearOnSend }: Pr
     const onQuestionChange = (_ev: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
         if (!newValue) {
             setQuestion("");
-        } else if (newValue.length <= 1000) {
+        } else if (newValue.length <= 2500) {
             setQuestion(newValue);
         }
     };

b4577386a1733a5dc994be343d78973cfb920a80
michael.jaumann <michael.jaumann@muenchen.de>
2023-07-27 18:14:36 +0200

    Besseres deutsch

--- app/frontend/src/components/TermsOfUseDialog/TermsOfUseDialog.tsx
+++ app/frontend/src/components/TermsOfUseDialog/TermsOfUseDialog.tsx
@@ -27,23 +27,23 @@ export const TermsOfUseDialog = () => {
             <DialogContent>
                 <ul>
                     <li>
-                    Keine Nutzung für Daten mit LHM-Schutzbedarf "hoch" oder "sehr hoch" oder mit der Klassifzierung "vertrauchlich" oder "streng vertraulich". Generell dürfen alle für Microsoft Azure zugelassenen Daten verarbeitet werden (gemäß Cloud Plattfon Security für Azure).
+                    Keine Nutzung für Daten mit LHM-Schutzbedarf "hoch" oder "sehr hoch" oder mit der Klassifizierung "vertrauchlich" oder "streng vertraulich". Generell dürfen alle für Microsoft Azure zugelassenen Daten verarbeitet werden.
                     </li>
                     <li>
-                    Einsatz nur für interne, betriebliche Tätigkeiten
+                    Einsatz nur für interne, betriebliche Tätigkeiten.
                     </li>
                     <li>
-                    KI-generierte Inhalte dürfen nicht extern veröffentlicht werden. KI-generierte Inhalte müssen bei interner unveränderter Nutzung als solche gekennzeichnet werden
+                    KI-generierte Inhalte dürfen nicht extern veröffentlicht werden. KI-generierte Inhalte müssen bei interner unveränderter Nutzung als solche gekennzeichnet werden.
                     </li>
                     <li>
-                    Achtung: MUCGPT ist darauf trainiert, immer zu antworten auch wenn es die Antwort nicht kennt.
-                    Dementsprechend entstehen sogenannte Halluzinationen, in jedem Anwendungsfall sind die Ergebnisse vor dem Einsatz manuell auf Korrektheit und Angemessenheit im LHM-Kontext zu überprüfen
+                    Achtung: MUCGPT ist darauf trainiert immer zu antworten, auch wenn es die Antwort nicht kennt.
+                    Dementsprechend entstehen sogenannte Halluzinationen. In jedem Anwendungsfall sind die Ergebnisse vor dem Einsatz manuell auf Korrektheit und Angemessenheit im LHM-Kontext zu überprüfen.
                     </li>
                     <li>
-                    Die Verantwortung für die Ergebnisse trägt bei der Weiterverwendung die/der MUCGPT Nutzer*in, KI-generierte Inhalte dürfen in keinem Fall zu einer automatisierten Entscheidungsfindung genutzt werden.
+                    Die Verantwortung für die Ergebnisse trägt bei der Weiterverwendung die/der MUCGPT Nutzer*in. KI-generierte Inhalte dürfen in keinem Fall zu einer automatisierten Entscheidungsfindung genutzt werden.
                     </li>
                     <li>
-                    Bei technischen Fehlern oder moralische Fehlverhalten der KI wendet euch bitte an itm.innolab@muenchen.de.
+                    Bei technischen Fehlern oder moralischem Fehlverhalten der KI wendet euch bitte an itm.innolab@muenchen.de.
                     </li>
                 </ul>
             </DialogContent>

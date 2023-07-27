645dc4d5a1dac0eef9b9b61c908e60bbdb288fd0
michael.jaumann <michael.jaumann@muenchen.de>
2023-07-27 12:26:56 +0200

    Farben angepasst

--- app/frontend/src/components/QuestionInput/QuestionInput.tsx
+++ app/frontend/src/components/QuestionInput/QuestionInput.tsx
@@ -61,7 +61,7 @@ export const QuestionInput = ({ onSend, disabled, placeholder, clearOnSend }: Pr
                     aria-label="Ask question button"
                     onClick={sendQuestion}
                 >
-                    <Send28Filled primaryFill="rgba(115, 118, 225, 1)" />
+                    <Send28Filled primaryFill={"rgba(255, 204, 0, 1)"} />
                 </div>
             </div>
         </Stack>

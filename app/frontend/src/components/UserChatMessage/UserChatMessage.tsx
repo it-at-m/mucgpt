ebf00b8025adf4589cc5a09290a0500bc43ae2a3
michael.jaumann <michael.jaumann@muenchen.de>
2023-11-03 13:08:23 +0100

    💄 weniger Abstand

--- app/frontend/src/components/UserChatMessage/UserChatMessage.tsx
+++ app/frontend/src/components/UserChatMessage/UserChatMessage.tsx
@@ -1,4 +1,8 @@
 import styles from "./UserChatMessage.module.css";
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw'
+import CodeBlockRenderer from "../CodeBlockRenderer/CodeBlockRenderer";
 
 interface Props {
     message: string;
@@ -7,7 +11,16 @@ interface Props {
 export const UserChatMessage = ({ message }: Props) => {
     return (
         <div className={styles.container}>
-            <div className={styles.message}>{message}</div>
+            <div className={styles.message}>          
+                 <Markdown 
+                    className={styles.answerText}
+                    remarkPlugins={[remarkGfm]} 
+                    rehypePlugins={[rehypeRaw]}
+                    children={message}
+                    components={{
+                        "code": CodeBlockRenderer
+                      }}/>
+            </div>
         </div>
     );
 };

e660fbf372d1c502ded031557a7d5847295b1eaf
michael.jaumann <michael.jaumann@muenchen.de>
2023-11-24 08:15:16 +0100

    FAQs aktualiisiert

--- app/frontend/src/components/TermsOfUseDialog/TermsOfUseDialog.tsx
+++ app/frontend/src/components/TermsOfUseDialog/TermsOfUseDialog.tsx
@@ -1,12 +1,12 @@
 import {
-  Dialog,
-  DialogTrigger,
-  DialogSurface,
-  DialogTitle,
-  DialogBody,
-  DialogActions,
-  DialogContent,
-  Button,
+    Dialog,
+    DialogTrigger,
+    DialogSurface,
+    DialogTitle,
+    DialogBody,
+    DialogActions,
+    DialogContent,
+    Button,
 } from "@fluentui/react-components";
 import { Checkmark24Filled } from '@fluentui/react-icons';
 import { useTranslation } from 'react-i18next';
@@ -14,50 +14,41 @@ import { useTranslation } from 'react-i18next';
 import styles from "./TermsOfUseDialog.module.css";
 
 export const TermsOfUseDialog = () => {
-  const { t} = useTranslation ();
-  return (
-    <div>
-        <Dialog modalType="alert" defaultOpen={true}>
-        <DialogTrigger disableButtonEnhancement>
-            <Button>{t('header.nutzungsbedingungen')}</Button>
-        </DialogTrigger>
-        <DialogSurface className={styles.dialog}>
-            <DialogBody  className={styles.dialogContent} >
-            <DialogTitle >{t('header.nutzungsbedingungen')}</DialogTitle>
-            <DialogContent>
-                <ul>
-                    <li>
-                    Keine Nutzung für Daten mit LHM-Schutzbedarf "hoch" oder "sehr hoch" oder mit der Klassifizierung "vertraulich" oder "streng vertraulich". Generell dürfen alle für Microsoft Azure zugelassenen Daten verarbeitet werden.
-                    </li>
-                    <li>
-                    Einsatz nur für interne, betriebliche Tätigkeiten.
-                    </li>
-                    <li>
-                    KI-generierte Inhalte dürfen nicht extern veröffentlicht werden. KI-generierte Inhalte müssen bei interner unveränderter Nutzung als solche gekennzeichnet werden.
-                    </li>
-                    <li>
-                    Achtung: MUCGPT ist darauf trainiert immer zu antworten, auch wenn es die Antwort nicht kennt.
-                    Dementsprechend entstehen sogenannte Halluzinationen. In jedem Anwendungsfall sind die Ergebnisse vor dem Einsatz manuell auf Korrektheit und Angemessenheit im LHM-Kontext zu überprüfen.
-                    </li>
-                    <li>
-                    Die Verantwortung für die Ergebnisse trägt bei der Weiterverwendung die/der MUCGPT Nutzer*in. KI-generierte Inhalte dürfen in keinem Fall zu einer automatisierten Entscheidungsfindung genutzt werden.
-                    </li>
-                    <li>
-                    Bei technischen Fehlern oder moralischem Fehlverhalten der KI wendet euch bitte an      <a href="mailto:itm.kicc@muenchen.de?subject=MUCGPT">
-                    itm.kicc@muenchen.de
-                </a>
-                    </li>
-                </ul>
-            </DialogContent>
-            <DialogActions>
+    const { t } = useTranslation();
+    return (
+        <div>
+            <Dialog modalType="alert" defaultOpen={true}>
                 <DialogTrigger disableButtonEnhancement>
-                <Button appearance="primary" size="small">
-                <Checkmark24Filled className={styles.checkIcon} />Zustimmen</Button>
+                    <Button>{t('header.nutzungsbedingungen')}</Button>
                 </DialogTrigger>
-            </DialogActions>
-            </DialogBody>
-        </DialogSurface>
-        </Dialog>
-    </div>
-  );
+                <DialogSurface className={styles.dialog}>
+                    <DialogBody className={styles.dialogContent} >
+                        <DialogTitle >Nutzungsbedingungen (Zustimmung erforderlich)</DialogTitle>
+                        <DialogContent>
+                            <ul>
+                                <li><strong>Zielgruppe:</strong> MUCGPT darf von allen städtischen Mitarbeiterinnen und Mitarbeitern und von allen Personen mit einem München-Account (muenchen.de Email-Adresse) verwendet werden.
+                                </li>
+                                <li><strong>Erlaubte Nutzung:</strong> MUCGPT darf nur im dienstlichen Kontext genutzt werden. Die Nutzung vergleichbarer kommerzieller KI-Produkte (z.B. ChatGPT, Google Bard etc.) ist nicht erlaubt. </li>
+                                <li><strong>Informationssicherheit und Datenschutz:</strong> Bei der Nutzung von MUCGPT dürfen keine personenbezogenen Daten (z.B. Name, Anschrift, E-Mail-Adressen etc.) oder als vertraulich klassifizierte Daten (siehe <a href="https://wilma.muenchen.de/pages/informationssicherheit/apps/wiki/regelwerk-aktuell/list/view/0b6737b2-9712-4fba-9fd3-a86f9daf98c5">Regelwerk Informationssicherheit</a>) eingeben werden.</li>
+                                <li><strong>Überprüfung der Ergebnisse:</strong> Die generierten Informationen bzw. Ergebnisse müssen stets auf inhaltliche Richtigkeit und Angemessenheit überprüft und bei Bedarf angepasst werden. Es kann vorkommen, dass MUCGPT Falschinformationen liefert („halluziniert“). Diese können zwar überzeugend klingen, sind aber inhaltlich falsch oder ungenau.</li>
+                                <li><strong>Weiterverwendung der Ergebnisse:</strong> Die Verantwortung für die Weiterverwendung der Ergebnisse trägt die/der MUCGPT Nutzer*in. Bei unveränderter bzw. nur geringfügig veränderter Nutzung müssen diese als solche gekennzeichnet werden.
+                                    Insbesondere, wenn die Inhalte für die externe Kommunikation verwendet werden. Siehe Details hierzu in den <a>FAQs</a>.</li>
+                                <li><strong>Entscheidungsfindung: </strong> MUCGPT darf nicht für abschließende Entscheidungen verwendet werden, die gegenüber Personen rechtliche Wirkung entfalten oder sie in ähnlicher Weise erheblich beeinträchtigen (Art. 22 Abs. 1 DSGVO). Derartige Entscheidungen müssen stets von Menschen getroffen werden</li>
+                                <li><strong>Ansprechpartner*innen:</strong> Bei Fragen und Feedback, sowie zum Melden von technischen Fehlern oder unangemessenen Ergebnissen von MUCGPT bitte an <a href="mailto:itm.kicc@muenchen.de?subject=MUCGPT"> itm.kicc@muenchen.de
+                                </a> wenden.</li>
+                                <li><strong>FAQs:</strong> Weitere Fragen und Antworten (u.a. zu Ziel und Einsatzzwecke von MUCGPT sowie zur Weiterverwendung der Eingaben und Ergebnisse) geben die <a>FAQs</a>.</li>
+                                <li><strong>Best-Practice Dokumentation/Wissensmanagement:</strong> Erfolgreiche Anwendungsfälle von MUCGPT sollen dokumentiert werden, um wertvolles Wissen für zukünftige Projekte zu generieren. Tragen Sie diese Beispiele gerne <a>hier</a> ein.</li>
+                            </ul>
+                        </DialogContent>
+                        <DialogActions>
+                            <DialogTrigger disableButtonEnhancement>
+                                <Button appearance="primary" size="small">
+                                    <Checkmark24Filled className={styles.checkIcon} />Zustimmen</Button>
+                            </DialogTrigger>
+                        </DialogActions>
+                    </DialogBody>
+                </DialogSurface>
+            </Dialog>
+        </div>
+    );
 };

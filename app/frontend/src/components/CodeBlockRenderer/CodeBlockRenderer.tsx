b915b5e608c9f64a40b582c6824aa23ab1ec7570
michael.jaumann <michael.jaumann@muenchen.de>
2023-12-11 14:23:28 +0100

    Fixes #66 Code wird besser formatiert, auch ohne angegebene Sprache

--- app/frontend/src/components/CodeBlockRenderer/CodeBlockRenderer.tsx
+++ app/frontend/src/components/CodeBlockRenderer/CodeBlockRenderer.tsx
@@ -2,49 +2,49 @@
 import { IconButton } from "@fluentui/react";
 import { ClassAttributes, HTMLAttributes, useState } from "react";
 import { ExtraProps } from "react-markdown";
-import {CopyToClipboard} from 'react-copy-to-clipboard';
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
-import {dark} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import styles from "./CodeBlockRenderer.module.css";
 
 export default function CodeBlockRenderer(props: ClassAttributes<HTMLElement> & HTMLAttributes<HTMLElement> & ExtraProps) {
-    const {children, className, node, ...rest} = props
-    const match = /language-(\w+)/.exec(className || '')
-    const [copied, setCopied] = useState<boolean>(false);
-    const [icon, setIcon] = useState<string>("Copy")
+  const { children, className, node, ...rest } = props
+  const match = /language-(\w+)/.exec(className || '')
+  const [copied, setCopied] = useState<boolean>(false);
+  const [icon, setIcon] = useState<string>("Copy")
 
-    const oncopy = () =>{
-        setCopied(true); 
-        setIcon("Checkmark"); 
-        setTimeout(()=> {
-            setIcon("Copy"); 
-            setCopied(false);
-        }, 1000)
-       }
-
-    return match ? (
-      <div className={styles.codeContainer}>
-          <SyntaxHighlighter
-          {...rest}
-          children={String(children).replace(/\n$/, '')}
-          style={dark}
-          language={match[1]}
-          PreTag="div"
-          />
-          <CopyToClipboard text={children}
-              onCopy={oncopy}>
-                      <IconButton
-                          style={{ color: "black"}}
-                          iconProps={{ iconName: icon}}
-                      >
-                      </IconButton>
-          </CopyToClipboard>
-      </div>
-    ) : (
-      <div>
-      <code {...rest} className={className}>
-        {children}
-      </code>
-      </div>
-    )
+  const oncopy = () => {
+    setCopied(true);
+    setIcon("Checkmark");
+    setTimeout(() => {
+      setIcon("Copy");
+      setCopied(false);
+    }, 1000)
   }
+
+  const language = match ? match[1] : "";
+  return (
+    <div className={styles.codeContainer}>
+      <SyntaxHighlighter
+        {...rest}
+        children={String(children).replace(/\n$/, '')}
+        style={dark}
+        language={language}
+        PreTag="div"
+        showLineNumbers={true}
+        wrapLongLines={true}
+      />
+      <div className={styles.copyContainer}>
+        {language}
+        <CopyToClipboard text={children}
+          onCopy={oncopy}>
+          <IconButton
+            style={{ color: "black" }}
+            iconProps={{ iconName: icon }}
+          >
+          </IconButton>
+        </CopyToClipboard>
+      </div>
+    </div>
+  )
+}

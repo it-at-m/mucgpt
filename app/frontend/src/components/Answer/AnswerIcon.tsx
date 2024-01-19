b9c35a7cc6e2d8c3b31f86e701c34a3ccf423dee
michael.jaumann <michael.jaumann@muenchen.de>
2024-01-19 21:32:00 +0100

    💄 Darüber anzeigen

--- app/frontend/src/components/Answer/AnswerIcon.tsx
+++ app/frontend/src/components/Answer/AnswerIcon.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 
 export const AnswerIcon = () => {
     const { t, i18n } = useTranslation();
-    return <Tooltip content={t('components.answericon.label')} relationship="description" positioning="below"  >
+    return <Tooltip content={t('components.answericon.label')} relationship="description" positioning="above"  >
         <BrainCircuit24Regular aria-hidden="true" aria-label={t('components.answericon.label')} />
     </Tooltip>;
 };

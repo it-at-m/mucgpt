5923557c7cde1ded1c8234193ad79f9ed7bbdbdf
michael.jaumann <michael.jaumann@muenchen.de>
2024-05-08 17:02:43 +0200

    Beispiele hinzugefügt

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -37,7 +37,57 @@ const EXAMPLES: ExampleModel[] = [
         - Antworte dabei ausschließlich in Markdown-Codeblöcken in der Programmiersprache mermaid
         - Beschrifte die Knoten der Diagramme passend
         - Verwende ausschließlich die Daten aus Schritt 1 und 2
-        
+
+        Eine Beispielausgabe aus Schritt 3 für ein Kuchendiagramm sieht so aus :
+        \`\`\`mermaid 
+        pie title Pets adopted by volunteers
+            "Dogs" : 386
+            "Cats" : 85
+            "Rats" : 15
+        \`\`\`
+
+        Eine Beispielausgabe aus Schritt 3 für eine Mindmap sieht so aus:
+        \`\`\`mermaid 
+        mindmap
+            root((mindmap))
+                Origins
+                    Long history
+                    ::icon(fa fa-book)
+                    Popularisation
+                    British popular psychology author Tony Buzan
+                Research
+                    On effectivness<br/>and features
+                    On Automatic creation
+                    Uses
+                        Creative techniques
+                        Strategic planning
+                        Argument mapping
+                Tools
+                    Pen and paper
+                    Mermaid
+        \`\`\`
+
+        Eine Beispielausgabe aus Schritt 3 für ein Sequenzdiagramm sieht so aus:
+        \`\`\`mermaid 
+        sequenceDiagram
+            Alice->>+John: Hello John, how are you?
+            Alice->>+John: John, can you hear me?
+            John-->>-Alice: Hi Alice, I can hear you!
+            John-->>-Alice: I feel great!
+         \`\`\`
+        Eine Beispielausgabe aus Schritt 3 für eine Userjourney sieht so aus:
+
+        \`\`\`mermaid 
+        journey
+            title My working day
+                section Go to work
+                    Make tea: 5: Me
+                    Go upstairs: 3: Me
+                    Do work: 1: Me, Cat
+            section Go home
+                Go downstairs: 5: Me
+                Sit down: 3: Me
+        \`\`\`   
         
         Starte mit Schritt 1.
         `

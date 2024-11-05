fefa6a4d579fded910b37166e78fab9131e1af2e
pilitz <102222789+pilitz@users.noreply.github.com>
2024-11-05 16:56:19 +0100

    :hammer: changed appearance

--- app/frontend/src/components/ChatSettingsButton/ChatSettingsButton.tsx
+++ app/frontend/src/components/ChatSettingsButton/ChatSettingsButton.tsx
@@ -14,12 +14,12 @@ export const ChatSettingsButton = ({ isEmptySystemPrompt, onClick }: Props) => {
         <div className={styles.container}>
             {isEmptySystemPrompt ?
                 <Tooltip content={t('components.chattsettingsdrawer.settings_button')} relationship="description" positioning="below">
-                    <Button aria-label={t('components.chattsettingsdrawer.settings_button')} icon={< ChatSettings24Regular />} appearance="subtle" onClick={onClick} size="large">
+                    <Button aria-label={t('components.chattsettingsdrawer.settings_button')} icon={< ChatSettings24Regular />} appearance="secondary" onClick={onClick} size="large">
                     </Button>
                 </Tooltip>
                 :
                 <Tooltip content={t('components.chattsettingsdrawer.settings_button_system_prompt')} relationship="description" positioning="below">
-                    <Button aria-label={t('components.chattsettingsdrawer.settings_button_system_prompt')} icon={<ChatWarning24Regular className={styles.system_prompt_warining_icon} />} appearance="subtle" onClick={onClick} size="large">
+                    <Button aria-label={t('components.chattsettingsdrawer.settings_button_system_prompt')} icon={<ChatWarning24Regular className={styles.system_prompt_warining_icon} />} appearance="secondary" onClick={onClick} size="large">
                     </Button>
                 </Tooltip>
             }

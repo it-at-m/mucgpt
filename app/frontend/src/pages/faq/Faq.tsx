44fb3f1b6efc965018bdd501621c1c8beeed93d3
pilitz <102222789+pilitz@users.noreply.github.com>
2025-02-20 11:25:08 +0100

    fixed typos

--- app/frontend/src/pages/faq/Faq.tsx
+++ app/frontend/src/pages/faq/Faq.tsx
@@ -32,7 +32,7 @@ const Faq = () => {
                         <AccordionPanel>
                             <div className={styles.panel}>
                                 MUCGPT ist ein Hilfsmittel, das unseren Alltag einfacher und effizienter gestalten kann. Es berechnet Wortwahrscheinlichkeiten
-                                mit Hilfe eines neuronalen Netzes, um Antworten zu generieren, die natürlich und plausibel klingen. Diese Antworten sind nicht
+                                mithilfe eines neuronalen Netzes, um Antworten zu generieren, die natürlich und plausibel klingen. Diese Antworten sind nicht
                                 reproduzierbar. Das bedeutet, dass die gleiche Anfrage zu einem anderen Zeitpunkt unterschiedliche Ergebnisse liefern kann.
                             </div>
                         </AccordionPanel>
@@ -41,7 +41,7 @@ const Faq = () => {
                         <AccordionHeader>Was ist die Basis von MUCGPT bzw. der Unterschied zu ChatGPT?</AccordionHeader>
                         <AccordionPanel>
                             <div className={styles.panel}>
-                                MUCGPT basiert auf ChatGPT, ein Dienst von OpenAI und Microsoft. Der Unterschied zu ChatGPT liegt jedoch darin, dass Ihre
+                                MUCGPT basiert auf ChatGPT, einem Dienst von OpenAI und Microsoft. Der Unterschied zu ChatGPT liegt jedoch darin, dass Ihre
                                 eingegebenen Daten verschlüsselt an einen europäischen Server von Microsoft Azure nach Amsterdam geschickt werden und nicht in
                                 die USA abfließen.
                             </div>
@@ -53,18 +53,18 @@ const Faq = () => {
                             <div className={styles.panel}>
                                 Ecosia und Google sind Suchmaschinen, die ans Internet angebunden sind. MUCGPT hat keine Live-Anbindung ans Internet, um sich
                                 von dort Informationen zu holen. MUCGPT ist ein Sprachmodell, welches für die Antwort die wahrscheinlichsten Wörter auf Basis
-                                der Nutzereingabe berechnet. Dahinter stecken Trainingsdaten, die unbekannt sind (wahrscheinlich sind das zum großen Teile Texte
+                                der Nutzereingabe berechnet. Dahinter stecken Trainingsdaten, die unbekannt sind (wahrscheinlich sind das zum großen Teil Texte
                                 aus dem Internet, aber auch Bücher etc.). Dies führt auch zu falschen Antworten, da das Sprachmodell auch Antworten erfindet,
                                 die gut in den Kontext passen.
                                 <p>
-                                    Google hingegen durchsucht regelmäßig alle Webseiten und indexiert diese in einer Art Datenbank. Bei einer Google Suche
+                                    Google hingegen durchsucht regelmäßig alle Webseiten und indexiert diese in einer Art Datenbank. Bei einer Google-Suche
                                     wird, vereinfacht gesagt, dann diese Datenbank nach dem passenden Treffer durchsucht und zurückgegeben. Wenn man auf der
                                     Suche nach Fakten ist, sollte man Suchmaschinen wie Google, Ecosia etc. verwenden, da diese genauere und vertrauenswürdigere
                                     Treffer liefern.
                                 </p>
                                 <p>
-                                    MUCGPT generiert Wort für Wort aus den Eingaben. Da dahinter ein großes Sprachmodell ist, was auf einem Server mit vielen
-                                    Grafikkarten läuft, ist eine Anfrage "energetisch" teurer, als eine Google Suche.
+                                    MUCGPT generiert Wort für Wort aus den Eingaben. Da dahinter ein großes Sprachmodell ist, das auf einem Server mit vielen
+                                    Grafikkarten läuft, ist eine Anfrage "energetisch" teurer als eine Google-Suche.
                                 </p>
                             </div>
                         </AccordionPanel>
@@ -73,7 +73,7 @@ const Faq = () => {
                         <AccordionHeader>Wer darf MUCGPT nutzen?</AccordionHeader>
                         <AccordionPanel>
                             <div className={styles.panel}>
-                                MUCGPT darf von allen städtischen Mitarbeitern*innen inkl. externer Dienstleister*innen (alle die einen muenchen.de-Account
+                                MUCGPT darf von allen städtischen Mitarbeiter*innen inkl. externer Dienstleister*innen (alle, die einen muenchen.de-Account
                                 besitzen) im dienstlichen Kontext genutzt werden.
                             </div>
                         </AccordionPanel>
@@ -83,7 +83,7 @@ const Faq = () => {
                         <AccordionPanel>
                             <div className={styles.panel}>
                                 MUCGPT kann als Ideengeber, Inspirationsquelle oder Diskussionspartner dienen oder Hilfe bei der Vorstrukturierung und
-                                Zusammenfassung von Texten geben. Folgende Ziele können verfolgt werden:{" "}
+                                Zusammenfassung von Texten geben. Folgende Ziele können verfolgt werden:
                                 <ul>
                                     <li>Erkenntnis-/Ideengewinnung</li>
                                     <li>Effizienzsteigerungen</li>
@@ -99,12 +99,12 @@ const Faq = () => {
                                 <div>
                                     <ul>
                                         <li>
-                                            <strong>Chat:</strong> Diese Funktion bietet den Nutzer*innen die Möglichkeit MUCGPT Fragen zu stellen, nach
+                                            <strong>Chat:</strong> Diese Funktion bietet den Nutzer*innen die Möglichkeit, MUCGPT Fragen zu stellen, nach
                                             Informationen zu suchen und verschiedene Themen zu diskutieren.
                                         </li>
                                         <li>
-                                            <strong>Zusammenfassen:</strong> Diese Funktion ermöglicht es MUCGPT prägnante Zusammenfassungen von Themen zu
-                                            erstellen, entweder auf der Basis von Eingaben, oder hochgeladenen PDFs.
+                                            <strong>Zusammenfassen:</strong> Diese Funktion ermöglicht es MUCGPT, prägnante Zusammenfassungen von Themen zu
+                                            erstellen, entweder auf der Basis von Eingaben oder hochgeladenen PDFs.
                                         </li>
                                         <li>
                                             <strong>Brainstorming:</strong> Diese Funktion generiert kreative Vorschläge, Ideen und Lösungen auf der Basis von
@@ -112,7 +112,7 @@ const Faq = () => {
                                         </li>
                                     </ul>
                                     <p>
-                                        Ein Erklärvideo zu den einzelnen Funktionen mit jeweiligen Beispielen, finden Sie{" "}
+                                        Ein Erklärvideo zu den einzelnen Funktionen mit jeweiligen Beispielen finden Sie{" "}
                                         <Link href="https://www.youtube.com/watch?v=jLFvdJhRV_U">hier</Link>.
                                     </p>
                                     <p>
@@ -123,12 +123,12 @@ const Faq = () => {
                                         </Link>
                                     </p>
                                     <p>
-                                        Um die in der Brainstorming Funktion generierten Mindmaps nach dem Abspeichern öffnen zu können, nutzen Sie am besten
+                                        Um die in der Brainstorming-Funktion generierten Mindmaps nach dem Abspeichern öffnen zu können, nutzen Sie am besten
                                         „Freeplane“, welches Sie in Service Now unter diesem{" "}
                                         <Link href="https://it-services.muenchen.de/sp?id=sc_cat_item&table=sc_cat_item&sys_id=c1b4dc4f1ba12154a70c433c8b4bcba0">
                                             Link
                                         </Link>{" "}
-                                        beantragen können (falls noch nicht auf Ihrem Rechner vorhanden).{" "}
+                                        beantragen können (falls noch nicht auf Ihrem Rechner vorhanden).
                                     </p>
                                 </div>
                             </div>
@@ -195,13 +195,13 @@ const Faq = () => {
                                 veröffentlicht, begeht damit – ohne es beabsichtigt zu haben – eine Urheberrechtsverletzung. Der Urheber des Originals kann in
                                 einem solchen Fall Unterlassungs-, Schadensersatz- und Beseitigungsansprüche geltend machen. Bei jeglicher Nutzung der
                                 Ergebnisse (egal ob intern oder extern), muss daher eine Kennzeichnung erfolgen („Quelle: MUCGPT“). Die Verantwortung für die
-                                Weiterverwendung der Ergebnisse trägt die/der MUCGPT Nutzer*in.
+                                Weiterverwendung der Ergebnisse trägt der/die MUCGPT-Nutzer*in.
                                 <p>
                                     Weiterhin muss beachtet werden, dass die Ergebnisse entsprechend dem jeweiligen Rechtsgebiet angemessen weiterverwendet
                                     werden. Das bedeutet, dass es (je nach betroffenem Rechtsgebiet) wichtig ist, nahe am üblichen Standard der Formulierungen
                                     zu bleiben, damit hier keine Ungleichheit oder Unverständlichkeit z.B. aufgrund von (durch die KI) anders gewählten
-                                    Fachbegriffen o.ä. entsteht. Bei der Nutzung von solch künstlich erzeugten Ergebnissen, sollte immer auch der gesunde
-                                    Menschenverstand angewendet werden, damit die Adressaten dieser Texte diese sowohl verstehen, als auch sich darauf verlassen
+                                    Fachbegriffen o.ä. entsteht. Bei der Nutzung von solch künstlich erzeugten Ergebnissen sollte immer auch der gesunde
+                                    Menschenverstand angewendet werden, damit die Adressaten dieser Texte diese sowohl verstehen als auch sich darauf verlassen
                                     können.
                                 </p>
                             </div>
@@ -228,7 +228,7 @@ const Faq = () => {
                             <div className={styles.panel}>
                                 Es ist nicht erlaubt, Daten mit LHM-Schutzbedarf "hoch" oder "sehr hoch" oder mit der Klassifizierung "vertraulich" oder "streng
                                 vertraulich" als Eingabe zu verwenden. Es dürfen keine personenbezogenen Daten (z.B. Name, Adresse, Telefonnummer,
-                                E-Mailadressen etc.) eingegeben werden. Weiterhin dürfen keine Zahlungsdaten (IBAN, BIC, Zahlungsempfänger, Vertragspartner
+                                E-Mail-Adressen etc.) eingegeben werden. Weiterhin dürfen keine Zahlungsdaten (IBAN, BIC, Zahlungsempfänger, Vertragspartner
                                 etc.) eingegeben werden.
                                 <p>
                                     Bitte beachten Sie, dass auch bei Nutzung von MUCGPT die Vorschriften der LHM (z.B.{" "}
@@ -244,7 +244,7 @@ const Faq = () => {
                         <AccordionHeader>Werden meine Eingaben irgendwo gespeichert? Wer hat Zugriff auf die Daten?</AccordionHeader>
                         <AccordionPanel>
                             <div className={styles.panel}>
-                                Ihre Eingaben werden nicht gespeichert. Keiner hat Zugriff auf Ihre eingegebenen Daten, sowie die generierten Antworten.
+                                Ihre Eingaben werden nicht gespeichert. Keiner hat Zugriff auf Ihre eingegebenen Daten sowie die generierten Antworten.
                             </div>
                         </AccordionPanel>
                     </AccordionItem>
@@ -253,7 +253,7 @@ const Faq = () => {
                         <AccordionPanel>
                             <div className={styles.panel}>
                                 Die gespeicherten Chats in Ihrer Historie werden lokal in Ihrem Browser gespeichert (IndexedDB). Dadurch sind die Chats nur für
-                                Sie zugänglich und werden nicht in der Cloud oder auf einem Server gespeichert, allerdings sind somit die Chats auch nur auf
+                                Sie zugänglich und werden nicht in der Cloud oder auf einem Server gespeichert. Allerdings sind somit die Chats auch nur auf
                                 diesem Gerät und diesem Browser zugänglich. Bei einem Wechsel auf ein anderes Gerät, einen anderen Browser oder in einen
                                 Inkognito-Tab sind die gespeicherten Chats nicht mehr verfügbar.
                             </div>

183619e84fde100d84f8754ce63562e866e93bac
michael.jaumann <michael.jaumann@muenchen.de>
2025-03-04 11:17:02 +0100

    🚑 fix tests

--- app/backend/core/helper.py
+++ app/backend/core/helper.py
@@ -33,8 +33,8 @@ def llm_exception_handler(ex: Exception, logger: Logger) -> str:
     Returns:
         str: the error message
     """
-    msg = ex.message
     try:
+        msg = ex.message
         if hasattr(ex, 'status_code') and hasattr(ex, 'code') and ex.status_code == 400 and ex.code == "content_filter":
             msg = "Es wurde ein Richtlinienverstoß festgestellt und der Chat wird hier beendet"
         elif hasattr(ex, 'status_code') and ex.status_code == 429:
@@ -44,5 +44,5 @@ def llm_exception_handler(ex: Exception, logger: Logger) -> str:
         logger.error("Chat error details: %s", vars(ex) if hasattr(ex, '__dict__') else str(ex))
     except Exception as nested_ex:
         msg = "Es ist ein unbekannter Fehler aufgetreten."
-        logger.error("Error while handling chat exception: %s", nested_ex)
+        logger.error("Error while handling exception: %s", nested_ex)
     return msg

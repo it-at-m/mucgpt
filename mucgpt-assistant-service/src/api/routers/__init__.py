cd51ce333c655babe83e5b4703513ebda2525c3f
michael.jaumann <michael.jaumann@muenchen.de>
2025-06-28 15:14:39 +0200

    💄 renaming for clarity

--- mucgpt-assistant-service/src/api/routers/__init__.py
+++ mucgpt-assistant-service/src/api/routers/__init__.py
@@ -1,5 +1,5 @@
-from .assistants import router as assistants_router
-from .system import router as system_router
-from .users import router as users_router
+from .assistants_router import router as assistants_router
+from .system_router import router as system_router
+from .users_router import router as users_router
 
 __all__ = ["assistants_router", "system_router", "users_router"]

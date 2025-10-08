a3acedb91584016a89d51bf30d0920a79b463d62
michael.jaumann <michael.jaumann@muenchen.de>
2025-10-08 16:38:21 +0200

    👌 review, use router for routing

--- mucgpt-frontend/src/components/TutorialsButton/TutorialsButton.tsx
+++ mucgpt-frontend/src/components/TutorialsButton/TutorialsButton.tsx
@@ -1,17 +1,18 @@
 import { Button, Tooltip } from "@fluentui/react-components";
 import { Book24Regular } from "@fluentui/react-icons";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 export const TutorialsButton = () => {
     const { t } = useTranslation();
 
     return (
         <Tooltip content={t("menu.go_to_tutorials_tooltip", "Tutorials und Anleitungen zu Fragments und Tools")} relationship="description" positioning="below">
-            <a href={"#/tutorials"}>
+            <Link to="/tutorials">
                 <Button appearance={"subtle"} icon={<Book24Regular />} aria-label={t("menu.go_to_tutorials_aria", "Zu Tutorials und Anleitungen navigieren")}>
                     {t("menu.go_to_tutorials", "Tutorials")}
                 </Button>
-            </a>
+            </Link>
         </Tooltip>
     );
 };

0061ccc17a96a23a609965aeeecc68d7891f5b6a
michael.jaumann <michael.jaumann@muenchen.de>
2025-10-09 10:52:00 +0200

    👌 small improvements to accesability

--- mucgpt-frontend/src/components/TutorialsButton/TutorialsButton.tsx
+++ mucgpt-frontend/src/components/TutorialsButton/TutorialsButton.tsx
@@ -1,18 +1,22 @@
 import { Button, Tooltip } from "@fluentui/react-components";
 import { Book24Regular } from "@fluentui/react-icons";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const TutorialsButton = () => {
     const { t } = useTranslation();
+    const navigate = useNavigate();
 
     return (
         <Tooltip content={t("menu.go_to_tutorials_tooltip", "Tutorials und Anleitungen zu Fragments und Tools")} relationship="description" positioning="below">
-            <Link to="/tutorials">
-                <Button appearance={"subtle"} icon={<Book24Regular />} aria-label={t("menu.go_to_tutorials_aria", "Zu Tutorials und Anleitungen navigieren")}>
-                    {t("menu.go_to_tutorials", "Tutorials")}
-                </Button>
-            </Link>
+            <Button
+                appearance={"subtle"}
+                icon={<Book24Regular />}
+                aria-label={t("menu.go_to_tutorials_aria", "Zu Tutorials und Anleitungen navigieren")}
+                onClick={() => navigate("/tutorials")}
+            >
+                {t("menu.go_to_tutorials", "Tutorials")}
+            </Button>
         </Tooltip>
     );
 };

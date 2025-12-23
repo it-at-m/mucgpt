e7bd9c0c0f71109c0f16f17cf6eab5c19f52b43d
michael.jaumann <michael.jaumann@muenchen.de>
2025-12-23 16:35:09 +0100

    💄 more directly visible buttons.

--- mucgpt-frontend/src/components/QuickPrompt/QuickPromptList.tsx
+++ mucgpt-frontend/src/components/QuickPrompt/QuickPromptList.tsx
@@ -20,7 +20,7 @@ export const QuickPromptList = ({ setQuestion }: Props) => {
             <div className={styles.buttons}>
                 {quickPrompts.map(quickPrompt => (
                     <Tooltip content={quickPrompt.tooltip} relationship="description" positioning="above" key={quickPrompt.label}>
-                        <Button onClick={() => setQuestion(quickPrompt.prompt)} appearance="subtle" aria-label={quickPrompt.prompt} className={styles.item}>
+                        <Button onClick={() => setQuestion(quickPrompt.prompt)} appearance="secondary" aria-label={quickPrompt.prompt} className={styles.item}>
                             {quickPrompt.label}
                         </Button>
                     </Tooltip>

4bb265521ad12731869ec5a311c2f7a99bce26f0
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2026-05-13 18:04:05 +0200

    added alternative logo again

--- mucgpt-frontend/src/pages/layout/Layout.tsx
+++ mucgpt-frontend/src/pages/layout/Layout.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from "react-i18next";
 
 import styles from "./Layout.module.css";
 import logo from "../../assets/edelweiss_logo_white.png";
+import alternative_logo from "../../assets/mugg_tschibidi.png";
 import logo_black from "../../assets/edelweiss_logo_black.png";
 import { DEFAULTLANG, LanguageContext } from "../../components/LanguageSelector/LanguageContextProvider";
 import { TermsOfUseDialog } from "../../components/TermsOfUseDialog";
